93f28c25e3385e2fe71c8da295d43463e545240f
vasimlook <vasimlook@gmail.com>
2020-11-20 16:21:01 +0530

    testimonial
    testimonial

--- _partials/testimonials.php
+++ _partials/testimonials.php
@@ -7,7 +7,7 @@
             <div>
                 <h1 class="testimonial-title">Testimonials</h1>
             </div>
-            <div class="col-md-2 col-sm-4 col-xs-6">
+            <div class="col-md-3 col-sm-4 col-xs-6">
                 <div class="partner-item">
                     <a href="images/testimonials/testimonial-576x1024-001.jpg" data-fancybox data-caption="Aranyavilas Testimonials from Guests">
                         <img src="images/testimonials/testimonial-67x120-001.jpg" alt="Testimonial 1" />
@@ -19,56 +19,94 @@
                 </div> <!-- /.partner-item -->
 
             </div> <!-- /.col-md-2 -->
-            <div class="col-md-2 col-sm-4 col-xs-6">
+            <div class="col-md-3 col-sm-4 col-xs-6">
                 <div class="partner-item">
                     <a href="images/testimonials/testimonial-576x1024-002.jpg" data-fancybox data-caption="Aranyavilas Testimonials from Guests">
-                        <img src="images/testimonials/testimonial-67x120-002.jpg" alt="Testimonial 2" />
-                    </a>
-                    <br />
-                    <div class="testimonial-link text-center">
-                        <h5>Click to Read</h5>
-                    </div>
+                        <img src="images/testimonials/testimonial-67x120-002.jpg" alt="Testimonial 2" />                    
+                        <br />
+                        <div class="testimonial-link text-center">
+                            <h5>Click to Read</h5>
+                        </div>
                 </div> <!-- /.partner-item -->
-
+                </a>
             </div> <!-- /.col-md-2 -->
-            <div class="col-md-2 col-sm-4 col-xs-6">
+            <div class="col-md-3 col-sm-4 col-xs-6">
                 <div class="partner-item">
                     <a href="images/testimonials/testimonial-576x1024-003.jpg" data-fancybox data-caption="Aranyavilas Testimonials from Guests">
                         <img src="images/testimonials/testimonial-67x120-003.jpg" alt="Testimonial 3" />
-                    </a>
-                    <br />
-                    <div class="testimonial-link text-center">
-                        <h5>Click to Read</h5>
-                    </div>
-                </div> <!-- /.partner-item -->
 
+                        <br />
+                        <div class="testimonial-link text-center">
+                            <h5>Click to Read</h5>
+                        </div>
+                </div> <!-- /.partner-item -->
+                </a>
             </div> <!-- /.col-md-2 -->
-            <div class="col-md-2 col-sm-4 col-xs-6">
+            <div class="col-md-3 col-sm-4 col-xs-6">
                 <div class="partner-item">
                     <a href="images/testimonials/testimonial-576x1024-004.jpg" data-fancybox data-caption="Aranyavilas Testimonials from Guests">
                         <img src="images/testimonials/testimonial-67x120-004.jpg" alt="Testimonial 4" />
-                    </a>
-                    <br />
-                    <div class="testimonial-link text-center">
-                        <h5>Click to Read</h5>
-                    </div>
-                </div> <!-- /.partner-item -->
 
+                        <br />
+                        <div class="testimonial-link text-center">
+                            <h5>Click to Read</h5>
+                        </div>
+                </div> <!-- /.partner-item -->
+                </a>
             </div> <!-- /.col-md-2 -->
-            <div class="col-md-2 col-sm-4 col-xs-6">
+            <div class="col-md-3 col-sm-4 col-xs-6">
                 <div class="partner-item">
                     <a href="images/testimonials/testimonial-576x1024-005.jpg" data-fancybox data-caption="Aranyavilas Testimonials from Guests">
                         <img src="images/testimonials/testimonial-67x120-005.jpg" alt="Testimonial 5" />
-                    </a>
-                    <br />
-                    <div class="testimonial-link text-center">
-                        <h5>Click to Read</h5>
-                    </div>
-                </div> <!-- /.partner-item -->
 
+                        <br />
+                        <div class="testimonial-link text-center">
+                            <h5>Click to Read</h5>
+                        </div>
+                </div> <!-- /.partner-item -->
+                </a>
 
             </div> <!-- /.col-md-2 -->
 
+            <div class="col-md-3 col-sm-4 col-xs-6">
+                <div class="partner-item">
+                    <a href="images/testimonials/testimonial-576x1024-006.jpg" data-fancybox data-caption="Aranyavilas Testimonials from Guests">
+                        <img src="images/testimonials/testimonial-67x120-006.jpg" alt="Testimonial 6" />
+
+                        <br />
+                        <div class="testimonial-link text-center">
+                            <h5>Click to Read</h5>
+                        </div>
+                </div> <!-- /.partner-item -->
+                </a>
+
+            </div> <!-- /.col-md-2 -->
+             <div class="col-md-3 col-sm-4 col-xs-6">
+                <div class="partner-item">
+                    <a href="images/testimonials/testimonial-576x1024-007.jpg" data-fancybox data-caption="Aranyavilas Testimonials from Guests">
+                        <img src="images/testimonials/testimonial-67x120-007.jpg" alt="Testimonial 7" />
+
+                        <br />
+                        <div class="testimonial-link text-center">
+                            <h5>Click to Read</h5>
+                        </div>
+                </div> <!-- /.partner-item -->
+                </a>
+
+            </div> <!-- /.col-md-2 -->
+            <div class="col-md-3 col-sm-3 col-xs-6">
+                <div class="partner-item">
+                    <a href="images/testimonials/testimonial-576x1024-008.jpg" data-fancybox data-caption="Aranyavilas Testimonials from Guests">
+                        <img src="images/testimonials/testimonial-67x120-008.jpg" alt="Testimonial 8" />
+
+                        <br />
+                        <div class="testimonial-link text-center">
+                            <h5>Click to Read</h5>
+                        </div>
+                </div> <!-- /.partner-item -->
+                </a>
+
+            </div> <!-- /.col-md-2 -->
         </div> <!-- /.row -->
     </div> <!-- /.container -->
 </div> <!-- /.partner-list -->
