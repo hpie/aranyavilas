aa024f44ab89094a197d14ce929197e2eb3ebe30
vasimlook <vasimlook@gmail.com>
2020-11-22 18:20:59 +0530

    chanes
    chanes

--- _partials/footer.php
+++ _partials/footer.php
@@ -69,17 +69,25 @@
 			});
 
 		</script>
-                <script type="text/javascript">
+                
+                
+                
+                <!-- GetButton.io widget -->
+<script type="text/javascript">
     (function () {
         var options = {
+            facebook: "106504124180087", // Facebook page ID
             whatsapp: "919857008200", // WhatsApp number
             call_to_action: "Inquiry Now!", // Call to action
-            position: "right" // Position may be 'right' or 'left'
+            button_color: "#A8CE50", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+            order: "facebook,whatsapp", // Order of buttons
         };
-        var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
         var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
         s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
         var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
     })();
 </script>
+<!-- /GetButton.io widget -->               
  
