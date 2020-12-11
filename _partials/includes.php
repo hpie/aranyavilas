2458bed6a199d6bf99012b74c498ff59caf1fb84
vasimlook <vasimlook@gmail.com>
2020-12-11 20:50:37 +0530

    google analytics code
    google analytics code

--- _partials/includes.php
+++ _partials/includes.php
@@ -70,5 +70,17 @@
             }
         }
        
-    </style>           
+    </style> 
+    
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-184191005-1">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-184191005-1');
+</script>
 </head>
